a748ca84af9878638ec1150e01e763fa8549d690
paul.borjesson.sesma <paul.borjesson.sesma@gmail.com>
2021-11-18 20:03:42 +0100

    DEMO1

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+//a
+
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
